d3942dde54c4ce34f15596ac55c4facd577f5b97
thefunkygibbon <github@thefunkygibbon.net>
2026-01-27 16:26:54 +0000

    fix __init

--- custom_components/eafm2/sensor.py
+++ custom_components/eafm2/sensor.py
@@ -1,44 +1,22 @@
-"""Support for Environment Agency Flood Monitoring sensors."""
-from datetime import timedelta
+"""The Environment Agency Flood Gauges Fixed integration."""
 import logging
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
-from . import aioeafm_local as aioeafm
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
-SCAN_INTERVAL = timedelta(minutes=15)
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up the sensors."""
-    session = async_get_clientsession(hass)
-    station_ref = entry.data["station"]
+# This tells Home Assistant to look for sensor.py
+PLATFORMS = [Platform.SENSOR]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up EAFM2 from a config entry."""
+    _LOGGER.debug("Setting up eafm2 entry: %s", entry.title)
     
-    try:
-        station = await aioeafm.get_station(session, station_ref)
-        # Create a sensor for every 'measure' (Level, Flow, etc.) the station has
-        entities = [EafmSensor(session, station, measure) for measure in station.measures]
-        async_add_entities(entities, True)
-    except Exception as err:
-        _LOGGER.error("Failed to set up EAFM sensors: %s", err)
-        return False
+    # This line is the 'bridge' that tells HA to now go and run sensor.py
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
 
-class EafmSensor(SensorEntity):
-    """Representation of an EAFM sensor."""
-
-    def __init__(self, session, station, measure):
-        self._session = session
-        self._station = station
-        self._measure = measure
-        self._attr_name = f"{station.label} {measure.label}"
-        self._attr_unique_id = f"{station.station_reference}_{measure.label}"
-        self._state = None
-
-    @property
-    def native_value(self):
-        return self._state
-
-    async def async_update(self):
-        """Update is handled by the data returned in the initial fetch for now."""
-        # You can add logic here later to fetch specific readings
-        pass
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry when the user deletes the integration."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
